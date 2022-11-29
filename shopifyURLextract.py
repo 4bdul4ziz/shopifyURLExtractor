64ae89c068fb5f360ba65ec8aafc01ce24cec234
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-11-30 02:29:30 +0530

    init

--- shopifyURLextract.py
+++ shopifyURLextract.py
@@ -1,5 +1,7 @@
+from os import pread
 import re
 from urllib.request import urlopen
+from xmlrpc.client import getparser
 
 def input_url():
     url = input("Enter the url: ")
@@ -9,6 +11,5 @@ def main():
     html = urlopen(input_url()).read().decode('utf-8')
     shopify_shop = re.search(r'Shopify.shop = "([^"]+)"', html, re.IGNORECASE)
     print(shopify_shop)
-
 main()
 
